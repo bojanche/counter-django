676ad0c88df554109ee33dade375da452e9c7380
bojanradonjic@yahoo.com <Bojana2512>
2020-10-07 16:08:48 +0200

    fixed

--- counter/occupancy/consumer.py
+++ counter/occupancy/consumer.py
@@ -5,16 +5,27 @@ from channels.consumer import AsyncConsumer
 
 
 class CountConsumer(AsyncConsumer):
-    async def connect(self, event):
-        print("Connected ", event)
+    async def websocket_connect(self, event):
+        print("Connected ")
         await self.send({
             "type": "websocket.accept"
         })
+        # await asyncio.sleep(10)
+        # print("Sacu te zatvorim")
+        # await self.send({
+        #     "type": "websocket.close"
+        # })
+        await self.send({
+            "type": "websocket.send",
+            "text": "Hello world"
+        })
 
-    async def disconnect(self, event):
+    async def websocket_disconnect(self, event):
         print("Disconnected ", event)
-        pass
+        # await self.send({
+        #     "type": "websocket.close"
+        #     })
 
-    async def receive(self, event):
+    async def websocket_receive(self, event):
         message = 'Bojan Websocket'
         self.send(message)
